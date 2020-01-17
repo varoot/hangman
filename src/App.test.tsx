150f2aa8e00e8582bd6948a77197f8e99f87e8cf
Varoot Phasuthadol <varoot.phasuthadol@accenture.com>
2020-01-17 16:37:52 +0700

    Add mock for fetchWord

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import App from './App';
+import { fetchWord } from './store/word/actions';
+import { LoadStatus, WordActionType } from './store/word/types';
 import { render } from './testUtils';
 
+jest.mock('./store/word/actions');
+
 describe('App', () => {
+  beforeEach(() => {
+    (fetchWord as any).mockReturnValue({
+      type: WordActionType.SetWord,
+      payload: {
+        status: LoadStatus.Ok,
+        letters: [65, 66, 67],
+      },
+    });
+  });
+
   it('renders reset button', () => {
     const { getByText } = render(<App />);
     const button = getByText('Reset');
